b3b90d469b28810b4a849547c21b7a7270770067
chris19toper <ernestpramudya4@gmail.com>
2025-04-13 21:47:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear();
+  
+  const targetDate = new Date("2025-04-27T15:00:00").getTime();
+  
+     const x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = targetDate - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+      }, 1000)
+  }());
